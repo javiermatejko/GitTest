3ded3cac12b501db1f7c59c59a17b68965dcfeb8
javiermatejko <javiermatejko@hotmail.com>
2019-09-04 13:51:11 -0300

    Primer commit

---
+++ Producto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    class Producto
+    {
+        string nombre;
+        int precio;
+
+        public Producto()
+        {
+            nombre = "Cajon";
+            precio = 150;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 15;
+
+            Console.WriteLine(n);
+        }
+    }
+}
